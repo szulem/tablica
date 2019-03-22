19cefe6f8b37ae3087d3f266b89745d0b8ef3904
szulem <mirek@szul.eu>
2019-03-22 01:45:36 +0100

    Create Ads controller

---
+++ test/controllers/ads_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class AdsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get ads_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get ads_show_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get ads_new_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get ads_edit_url
+    assert_response :success
+  end
+
+end
