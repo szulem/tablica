cbd2ce21a1b3c11e6b9b9b61155c5087b598093d
szulem <mirek@szul.eu>
2019-04-09 00:36:29 +0200

    Paperclip sizes

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -1,6 +1,6 @@
 class Ad < ApplicationRecord
 	belongs_to :user
 
-  has_attached_file :picture, styles: { medium: "250x200>" }, default_url: "https://dummyimage.com/250x200/f4f4f4/000000.png"
+  has_attached_file :picture, styles: { medium: "250x200#", big: "750x600>" }, default_url: "https://dummyimage.com/250x200/f4f4f4/000000.png&text=tablica"
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
 end
