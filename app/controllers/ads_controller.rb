6a7f251485feaba563fdab1162dcc88cb16216c8
szulem <mirek@szul.eu>
2019-04-10 20:57:12 +0200

    Add authentication user to edit and destroy post

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -1,5 +1,7 @@
 class AdsController < ApplicationController
   before_action :set_ad, only: %i[show edit update destroy]
+  before_action :authenticate_user!, except: %i[index show]
+  before_action :ad_owner, only: %i[edit update destroy]
 
   def index
   	@ads = Ad.all.order("created_at DESC")
@@ -27,6 +29,7 @@ class AdsController < ApplicationController
   end
 
   def update
+    # @ad.user_id = current_user.id
     if @ad.update(ad_params)
       redirect_to @ad, notice: "Your ad was successfully updated."
     else
@@ -47,5 +50,12 @@ class AdsController < ApplicationController
 
   def ad_params
   	params.require(:ad).permit(:title, :category, :description, :localization, :phone, :price, :picture, :to_negotiation, :user_id)
+  end
+
+  def ad_owner
+    unless @ad.user_id == current_user.id
+      flash[:alert] = 'Nie masz uprawnień do edycji tego ogłoszenia.'
+      redirect_to root_path
+    end
   end
 end
