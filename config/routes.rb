64f24892d6f6c0135185904b56bea701ee981bd0
szulem <mirek@szul.eu>
2019-04-15 14:06:53 +0200

    Change the order of two users lines

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
   root "static#home"
-  resources :users, only: :show
   devise_for :users
+  resources :users, only: :show
   resources :ads
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
