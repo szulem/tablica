c6ce3be2e10a3daddcf60f816be9565dd722631e
szulem <mirek@szul.eu>
2019-04-16 16:39:33 +0200

    Display category and sort ads on user's show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
 	def show
 		@user = User.find(params[:id])
 		# @user_ads = @user.ads.all
-		@ads = @user.ads.all
+		@ads = @user.ads.all.order("created_at DESC")
 	end
 end
