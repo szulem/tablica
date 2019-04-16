cebf355cddb3920ef6a11f50751f06b93d348f52
szulem <mirek@szul.eu>
2019-04-16 11:23:52 +0200

    Add subcategories table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_16_090810) do
+ActiveRecord::Schema.define(version: 2019_04_16_092135) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,14 @@ ActiveRecord::Schema.define(version: 2019_04_16_090810) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "subcategories", force: :cascade do |t|
+    t.string "title"
+    t.bigint "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_subcategories_on_category_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -61,4 +69,5 @@ ActiveRecord::Schema.define(version: 2019_04_16_090810) do
   end
 
   add_foreign_key "ads", "users"
+  add_foreign_key "subcategories", "categories"
 end
